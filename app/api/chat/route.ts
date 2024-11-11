e2e12a9d63af34c6a56338e2a1aedd8b2ab88856
Charnjiv Brar <chruquantum@gmail.com>
2024-11-10 20:11:46 -0500

    findrelevantContext

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,7 +1,9 @@
 import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
-import { Reem_Kufi } from "next/font/google";
-import { NextRequest } from "next/server";
+import { streamText, tool } from "ai";
+import { z } from "zod";
+import { createResource } from "@/lib/actions/resources";
+import { findRelevantContent } from "@/lib/ai/embedding";
+
 export const maxDuration = 30;
 
 export async function POST(req: Request){
@@ -9,7 +11,35 @@ export async function POST(req: Request){
 
     const result = await streamText({
         model: openai("gpt-4o"),   
+        system: `You are a helpful assistant. Check your knowledge base before answering. 
+        Only respond to questions using information from tool calls
+        If no relevant information is found in the tools calls, respond "Sorry, I don't know.`,
         messages,
+        tools:{
+            addResource: tool({
+                description: `add a resource to your knowledge base.
+                if the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+                parameters: z.object({
+                    content: z
+                    .string()
+                    .describe(`the content of the resource to add to your knowledge base`),
+                    
+                }),
+                execute: async ({content}) => {
+                    await createResource({content});
+                }
+            }),
+            getInformation: tool({
+                description: `get information from your knowledge base to answer questions.`,
+                parameters: z.object({
+                    question: z.string().describe(`the user's question`),
+                }),
+                execute: async ({question}) => {
+                    return await findRelevantContent(question);
+                    
+                }
+            }),
+        }
     });
 
     return result.toDataStreamResponse();
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,34 @@
 'use client'
 import { useChat } from 'ai/react';
+import { Reem_Kufi, Roboto } from "next/font/google";
 
 
 export default function Chat() {
-  const {messages, input, handleInputChange , handleSubmit} = useChat();
+  const {messages, input, handleInputChange , handleSubmit} = useChat(
+    {
+      maxSteps: 3,
+    }
+  );
   return (
     <div className='flex flex-col w-full max-w-md py-24 mx-auto-stretch'>
       <div className ="space-y-4 flex-1 px-8"> 
         {messages.map(m=> (
           <div key={m.id} className="chat-message whitespace-pre-wrap">
             <div>
-              <div className="font-bold">
+              <div className="font-bold font-Roboto">
                 {m.role}
               </div>
-            <p>{m.content}</p>
+            <p className=" font-Roboto">
+            {m.content.length > 0 ? (
+              m.content
+            ) : (
+              <span className="italic font-light font-Roboto">
+                { 'calling tool: ' + m?.toolInvocations?.[0].toolName}
+              </span>
+            )}
+
+
+            </p>
            </div>
           </div>
           
